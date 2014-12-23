a197f79de51be2a584c026f2ec94ced93568e46a
hmhaley <mhaley@vmgstrategies.com>
2014-12-22 17:05:24 -0800

    fixed fonts not displaying properly in heroku by moving them from the at import method in css to the javascript method

---
+++ app/assets/javascripts/fonts.js
@@ -0,0 +1,37 @@
+WebFontConfig = {
+	google: { families: ['Dosis:400,700,800:latin', 'Exo+2:700,500italic,400:latin' ] }
+};
+
+(function() {
+	var wf = documnt.createElement('script');
+	wf.scr = ('https:' == document.location.protocol ? 'https' : 'http') +
+		'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	wf.type = 'text/javascript';
+	wf.async = 'true';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wf, s);
+})
+
+();
+
+
+/////   OTHER FONTS I LIKE /////
+
+// <link href="//fonts.googleapis.com/css?family=Unica+One:400" rel="stylesheet" type="text/css">
+// <link href="//fonts.googleapis.com/css?family=Open+Sans:300italic,300,400italic,400,600italic,600,700italic,700,800italic,800" rel="stylesheet" type="text/css">
+// <link href="//fonts.googleapis.com/css?family=Lato:100italic,100,300italic,300,400italic,400,700italic,700,900italic,900" rel="stylesheet" type="text/css">
+// <link href="//fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+// <link href="//fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet" type="text/css">
+
+
+
+// .page-wrap {
+//   min-height: 100%;
+//   margin-bottom: -60px;
+// }
+
+// .page-wrap:after {
+//   content: "";
+//   display: block;
+//   height: 60px
+// }
--- app/models/executive.rb
+++ app/models/executive.rb
@@ -14,20 +14,20 @@ include ActiveModel::SecurePassword
 
 	has_many :associations
 
-	validates_uniqueness_of :username
-	validates_length_of :username, minimum: 5, maximum: 20
-	validates_length_of :password, minimum: 5, maximum: 20
-	validates_length_of :password_digest, minimum: 5, maximum: 20
+	# validates_uniqueness_of :username
+	# validates_length_of :username, minimum: 5, maximum: 20
+	# validates_length_of :password, minimum: 5, maximum: 20
+	# validates_length_of :password_digest, minimum: 5, maximum: 20
 
-	validates_presence_of :prefix
-	validates_presence_of :firstname
-	validates_length_of :firstname, minimum: 1, maximum: 30
-	validates_presence_of :lastname
-	validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-	validates_length_of :email, maximum: 255
-	validates :email, uniqueness: { case_sensitive: false }
-	before_save { |executive| executive.email = executive.email.downcase }
-	validates_presence_of :mobile_tel
-	validates_presence_of :office_tel
+	# validates_presence_of :prefix
+	# validates_presence_of :firstname
+	# validates_length_of :firstname, minimum: 1, maximum: 30
+	# validates_presence_of :lastname
+	# validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+	# validates_length_of :email, maximum: 255
+	# validates :email, uniqueness: { case_sensitive: false }
+	# before_save { |executive| executive.email = executive.email.downcase }
+	# validates_presence_of :mobile_tel
+	# validates_presence_of :office_tel
 
 end
